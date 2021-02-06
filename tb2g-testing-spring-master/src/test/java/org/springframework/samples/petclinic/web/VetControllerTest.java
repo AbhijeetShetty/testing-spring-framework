32d90d2a22eebaf51a0eab668b7cb57ffb53807a
Abhijeet Shetty <abhijeetshetty001@gmail.com>
2021-02-06 18:05:23 +0100

    setting up mock mvc standalone setup

--- tb2g-testing-spring-master/src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
+++ tb2g-testing-spring-master/src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -19,43 +20,61 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @ExtendWith(MockitoExtension.class)
-class VetControllerTest {    @Mock
-    ClinicService clinicService;
+class VetControllerTest {
+	@Mock
+	ClinicService clinicService;
 
-    @Mock
-    Map<String, Object> model;
+	@Mock
+	Map<String, Object> model;
 
-    @InjectMocks
-    VetController controller;
+	@InjectMocks
+	VetController controller;
 
-    List<Vet> vetsList = new ArrayList<>();
+	List<Vet> vetsList = new ArrayList<>();
 
-    @BeforeEach
-    void setUp() {
-        vetsList.add(new Vet());
+	MockMvc mockMvc;
 
-        given(clinicService.findVets()).willReturn(vetsList);
-    }
+	@BeforeEach
+	void setUp() {
+		vetsList.add(new Vet());
 
-    @Test
-    void showVetList() {
-        //when
-        String view = controller.showVetList(model);
+		given(clinicService.findVets()).willReturn(vetsList);
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
 
-        //then
-        then(clinicService).should().findVets();
-        then(model).should().put(anyString(), any());
-        assertThat("vets/VetList").isEqualToIgnoringCase(view);
-    } 
- 
-    @Test
-    void showResourcesVetList() {
-        //when
-        Vets vets = controller.showResourcesVetList();
+	@Test
+	void testControllerShowVetList() throws Exception {
+		mockMvc.perform(get("/vets.html"))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("vets"))
+		.andExpect(view().name("vets/vetList"));
+	}
 
-        //then
-        then(clinicService).should().findVets();
-        assertThat(vets.getVetList()).hasSize(1);
-    }
-    }
+	@Test
+	void showVetList() {
+		// when
+		String view = controller.showVetList(model);
+
+		// then
+		then(clinicService).should().findVets();
+		then(model).should().put(anyString(), any());
+		assertThat("vets/VetList").isEqualToIgnoringCase(view);
+	}
+
+	@Test
+	void showResourcesVetList() {
+		// when
+		Vets vets = controller.showResourcesVetList();
+
+		// then
+		then(clinicService).should().findVets();
+		assertThat(vets.getVetList()).hasSize(1);
+	}
+}
